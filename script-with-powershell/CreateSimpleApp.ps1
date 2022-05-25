7a937e013c62e014afd60174ebe80b4dd09d76ac
Aislan Michel Moreira Freitas <aislan.michel92@gmail.com>
2022-05-24 21:48:31 -0300

    fun

---
+++ script-with-powershell/CreateSimpleApp.ps1
@@ -0,0 +1,6 @@
+mkdir app
+cd app
+touch index.html app.js
+cd ..
+
+Write-Host "App was created"
---
+++ script-with-powershell/DestroySimpleApp.ps1
@@ -0,0 +1,7 @@
+Param(
+    [string]$name = "app"
+)
+
+rmdir $name
+
+Write-Host "App was destroy"
